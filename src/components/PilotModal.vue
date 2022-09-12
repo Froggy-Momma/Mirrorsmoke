8964a008d05b35fe8038257c2a1e602846fe45fb
Maja <maja@froglet.studio>
2022-09-12 10:43:53 -0400

    Update PilotModal.vue

--- src/components/PilotModal.vue
+++ src/components/PilotModal.vue
@@ -19,8 +19,8 @@
 			</div>
 			<div class="rhombus-back">&nbsp;</div>
 		</div>
-		<div class="mech">
-			<img :src="mechPortrait" class="portrait" />
+		<div class="pilot">
+			<img :src="portrait" class="portrait" />
 		</div>
 	</div>
 </template>
